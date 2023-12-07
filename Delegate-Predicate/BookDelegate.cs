867a40722ab3a40bf5cb61a6b849127e086879f8
mariama1ieva <maryamfa@code.edu.az>
2023-12-07 18:50:59 +0400

    added task of DelegatePredicate

---
+++ Delegate-Predicate/BookDelegate.cs
@@ -0,0 +1,35 @@
+﻿using Delegate_Predicate.Models;
+
+namespace Delegate_Predicate
+{
+    internal class BookDelegate
+    {
+        public delegate bool CheckAuthor(List<Book> books);
+
+        public void ShowCountOfBook(List<Book> books, Predicate<Book> predicate)
+        {
+            int count = 0;
+            foreach (Book book in books)
+            {
+                if (predicate(book))
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+
+        }
+
+        public void Result()
+        {
+            List<Book> books = new List<Book>()
+            {
+              new Book() {Author="Nizami Gencevi", Name="Isgendername"},
+              new Book() {Author="Nizami Gencevi", Name="Isgendername"},
+              new Book() {Author="Mehemmed Fizuli", Name="Leyli ve Mecnun"},
+              new Book() {Author="Mehemmed Fizuli", Name="Divan-ı Füzuli"},
+            };
+            ShowCountOfBook(books, books => books.Author == "Nizami Gencevi");
+        }
+    }
+}
---
+++ Delegate-Predicate/EmployeeDelegate.cs
@@ -0,0 +1,33 @@
+﻿using Delegate_Predicate.Models;
+
+namespace Delegate_Predicate
+{
+    internal class EmployeeDelegate
+    {
+        public delegate bool CheckSalary(List<Person> person);
+        public void ShowPersons(List<Person> persons, Predicate<Person> predicate)
+        {
+            foreach (Person person in persons)
+            {
+                if (predicate(person))
+                {
+                    Console.WriteLine($"{person.Name} - {person.Surname} - {person.Address}");
+                }
+            }
+        }
+
+        public void Result()
+        {
+            List<Person> people = new List<Person>()
+            {
+            new Person() { Name = "Mariam", Surname = "Aliyeva", Salary = 2000, Address = "London" },
+            new Person() { Name = "Tofiq", Surname = "Nasibli", Salary = 2500, Address = "Canada" },
+            new Person() { Name = "Haciaga", Surname = "Ahmedov", Salary = 900, Address = "Newyork" },
+            new Person() { Name = "Arzu", Surname = "Karimli", Salary = 800, Address = "Baku" },
+
+            };
+
+            ShowPersons(people, people => people.Salary > 1000);
+        }
+    }
+}
